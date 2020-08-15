bb83873185c600f0e6df29f62ae85156c802305a
Luiz Picanço <lpicanco@gmail.com>
2020-08-15 19:08:13 -0300

    Sonar and jacoco configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "1.3.72"
     id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
-    id("org.sonarqube") version "3.0"
     jacoco
 }
 
@@ -32,13 +31,5 @@ tasks {
         reports {
             xml.isEnabled = true
         }
-    }
-}
-
-sonarqube {
-    properties {
-        property("sonar.projectKey", "lpicanco-chip-8-emulator")
-        property("sonar.junit.reportPaths", "build/test-results/test")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
     }
 }
